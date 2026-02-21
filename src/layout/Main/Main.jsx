5c579d2f65913656bbf0f9c10059043f4733e85d
thissova <ivan22dev@gmail.com>
2026-02-21 22:44:23 +0200

    fixed bug

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 
 const Main = () => {
   return (
-<<<<<<< HEAD
     <main>
       <Routes>
         <Route path="/about" element={''} />
@@ -11,15 +10,7 @@ const Main = () => {
         <Route path="/settings" element={''} />
       </Routes>
     </main>
-=======
-    <Routes>
-      <Route path="/about" element={''} />
-      <Route path="/chats" element={''} />
-      <Route path="/news" element={''} />
-      <Route path="/settings" element={''} />
-    </Routes>
->>>>>>> 31c9949fb92f1579810a2c64ea6347261f2d010c
   );
-}
+};
 
 export default Main;
